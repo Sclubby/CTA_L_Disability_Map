f103e662fadb45cb880d4c78cb3d93085e359154
Sclubby <seimi2@uic.edu>
2024-10-16 15:21:28 -0500

    added a search page to the tool bar and cleaned up some visual elements in the CSS and HTML

---
+++ Pages/Search.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CTA_Database_Website.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
